b8c000b84e4682cc864768a6189d8afa724743a6
Emjy <emjy.ahyong@gmail.com>
2025-05-15 11:24:59 +0400

    add Entité Client

--- src/main/java/com/quickorder/model/Client.java
+++ src/main/java/com/quickorder/model/Client.java
@@ -1,7 +1,17 @@
 package com.quickorder.model;
 
-public class Client {
+import jakarta.persistence.*;
 
+import java.io.Serializable;
+
+@Entity
+@Table(name = "client")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Client implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id",nullable = false)
     private Long id;
     private String nom;
     private String prenom;
@@ -11,4 +21,80 @@ public class Client {
     private Integer telephone;
     private String email;
 
+    public Client() {}
+
+    public Client(Long id, String nom, String prenom, String adresse, String ville, String codePostal, Integer telephone, String email) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.ville = ville;
+        this.codePostal = codePostal;
+        this.telephone = telephone;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public Integer getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(Integer telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
